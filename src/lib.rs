f90450316f8654e33b874f1ae4e023d3d9603ada
zeta12ti <zeta12ti@users.noreply.github.com>
2017-08-04 21:06:26 -0600

    added new examples at the crate level

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,17 @@
 //!
 //! The Checked struct allows you to perform checked arithmetic without an overabundance of
 //! .checked_* calls.
+//! # Examples
+//! ```
+//! use checked::Checked;
+//!
+//! let x = Checked::from(1_000_u32);
+//! let y = Checked::from(1_000_000_u32);
+//!
+//! assert_eq!(x * 1_000, y);
+//! assert_eq!(1_000_000 * y, Checked::from(None));
+//! assert_eq!(x << 3, Checked::<u32>::from(8_000));
+//! ```
 
 mod num;
 pub use num::Checked;
