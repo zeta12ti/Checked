7946d5c260e0a859346c16168b961775405ba6b3
zeta12ti <zeta12ti@users.noreply.github.com>
2017-08-05 03:53:53 -0600

    add lots more examples

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,34 @@
 //! assert_eq!(1_000_000 * y, Checked::from(None));
 //! assert_eq!(x << 3, Checked::<u32>::from(8_000));
 //! ```
+//!
+//! All the basic arithmetic operations are implemented.
+//! ```
+//! assert_eq!(Checked::<u8>::from(5) + Checked::<u8>::from(6), Checked::<u8>::from(11));
+//! assert_eq!(Checked::<u8>::from(5) - Checked::<u8>::from(6), Checked::<u8>::from(None));
+//! assert_eq!(Checked::<u8>::from(5) * Checked::<u8>::from(6), Checked::<u8>::from(30));
+//! assert_eq!(Checked::<u8>::from(5) / Checked::<u8>::from(6), Checked::<u8>::from(0));
+//! assert_eq!(- Checked::<u8>::from(6), Checked::<u8>::from(None));
+//! assert_eq!(Checked::<u8>::from(5) % Checked::<u8>::from(6), Checked::<u8>::from(5));
+//! ```
+//!
+//! Including bitwise operations.
+//! ```
+//! assert_eq!(Checked::<u8>::from(5) & Checked::<u8>::from(6), Checked::<u8>::from(4));
+//! assert_eq!(Checked::<u8>::from(5) | Checked::<u8>::from(6), Checked::<u8>::from(7));
+//! assert_eq!(Checked::<u8>::from(5) ^ Checked::<u8>::from(6), Checked::<u8>::from(3));
+//! assert_eq!(! Checked::<u8>::from(6), Checked::<u8>::from(249));
+//! assert_eq!(Checked::<u8>::from(5) << Checked::<u32>::from(5), Checked::<u8>::from(160));
+//! assert_eq!(Checked::<u8>::from(160) >> Checked::<u32>::from(5), Checked::<u8>::from(5));
+//! ```
+//!
+//! Ord is implemented, and the overflow state is considered less than everything
+//! (may change this in the future)
+//! ```
+//! assert!(Checked::from(1_000_u32) <= Checked::from(10_000_u32));
+//! assert!(!(Checked::from(1_000_u32) <= Checked::from(None)));
+//! assert!((Checked::from(None) <= Checked::from(1_000_u32)));
+//! ```
 
 mod num;
 pub use num::Checked;
